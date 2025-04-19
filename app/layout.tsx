47d9bcfd7435ae03a47f2346232e87208f0f9420
nfworking <tidalfarmer1432@outlook.com>
2025-04-19 17:22:11 +1000

    43:41

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./components/theme-provider"
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            <main className="max-w-2xl mx-auto px-4">{children}</main>
+      </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Navbar from "./components/Navbar";
+async function getData(){
+  const query = `
+  *[_type == 'blog'] | order(_createdAt desc) {
+    title,
+    smallDescription,
+    "currentSlug": slug.current
+  }`;
+
+  
+
+}
 
 export default function Home() {
   return (
   <div>
-   <Navbar />
   </div>
   );
 }
