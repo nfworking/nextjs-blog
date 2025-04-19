988039b15ee1b5c99ca038a6ff099d2bb548cba4
nfworking <tidalfarmer1432@outlook.com>
2025-04-19 16:54:46 +1000

    28:35

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     return (
         <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
          <Link href="/" className="font-bold text-3xl">
-         Cals<span className="text-green-500">Blog</span>
+         Cals<span className="text-primary">Blog</span>
          </Link>
         </nav>
     );
